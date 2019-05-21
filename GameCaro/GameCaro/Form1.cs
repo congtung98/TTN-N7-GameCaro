6a43fb78f8b6e93d994aff585ffc510d5cc0e157
unknown <congtunggv@gmail.com>
2019-05-21 22:48:18 +0700

    initial commit

--- GameCaro/GameCaro/Form1.cs
+++ GameCaro/GameCaro/Form1.cs
@@ -14,10 +14,18 @@ namespace GameCaro
 {
     public partial class Form1 : Form
     {
+        #region Properties
+        ChessBoardManager ChessBoard;
+
+
+        #endregion
         public Form1()
         {
             InitializeComponent();
 
+            ChessBoard = new ChessBoardManager(pnlChessBoard, txbPlayerName, pctbMark);
+
+            ChessBoard.DrawChessBoard();
         }
         
     }
